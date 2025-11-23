074c4e0a7affb6ade6dd6c9d2965801c6e18571d
Urielsousa <urielzinho@hotmail.com>
2025-11-23 19:14:56 -0300

    att map

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const list = document.querySelector("ul");
 const buttonShowAll = document.querySelector(".showAll");
+const buttonMapAll = document.querySelector(".map-all");
 let myLi = "";
 
-function showAll() {
+function showAll(productsArray) {
   myLi = "";
-  menuOptions.forEach((product) => {
+  productsArray.forEach((product) => {
     myLi += `<li class="">
         <img src="${product.src}" alt="${product.name}" />
         <p>${product.name}</p>
-        <p class="item-price">R$ ${product.price},00</p>
+        <p class="item-price">R$ ${product.price}</p>
       </li>
   `;
   });
   list.innerHTML = myLi;
 }
 
-buttonShowAll.addEventListener("click", showAll);
+function mapAll() {
+  const newPrice = menuOptions.map((product) => ({
+    ...product, //spreat
+    price: product.price * 0.9,
+  }));
+  showAll(newPrice);
+}
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions));
+buttonMapAll.addEventListener("click", mapAll);
